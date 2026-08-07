30d8c8c1513f3298344f62c6599da308b7284f8d
igor-vii <auvip@mail.ru>
2026-08-07 16:46:21 +0000

    fix(contract): update OpenZeppelin v5 import path for Pausable

--- contracts/src/ZeusInsuranceV2.sol
+++ contracts/src/ZeusInsuranceV2.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "./ZeusReserveV2.sol";
